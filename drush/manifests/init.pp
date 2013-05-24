098ab3f487296c28f11684d57ad8deb3ef988019
Rob Parrott <robparrott@gmail.com>
2013-05-23 21:42:34 -0400

    update version of drush

--- drush/manifests/init.pp
+++ drush/manifests/init.pp
@@ -1,6 +1,6 @@
 class drush {
 
-  $drush_branch_name = "7.x-4.x"
+  $drush_branch_name = "7.x-5.9"
   $drush_make_branch_name = "6.x-2.x"
 
   exec {
@@ -8,10 +8,10 @@ class drush {
       cwd     => '/tmp',
       command => "/usr/bin/git clone -q --branch $drush_branch_name http://git.drupal.org/project/drush.git",
       creates => '/tmp/drush';
-    'fetch-drush-make':
-        cwd     => '/tmp',
-        command => "/usr/bin/git clone -q --branch $drush_make_branch_name http://git.drupal.org/project/drush_make.git",
-        creates => '/tmp/drush_make';
+#    'fetch-drush-make':
+#        cwd     => '/tmp',
+#        command => "/usr/bin/git clone -q --branch $drush_make_branch_name http://git.drupal.org/project/drush_make.git",
+#        creates => '/tmp/drush_make';
     'run-drush' :
       cwd     => '/tmp',
       command => "/usr/local/lib/drush/drush",
@@ -32,12 +32,12 @@ class drush {
       purge   => true,
       source  => '/tmp/drush',
       require => Exec['fetch-drush'];            
-    '/usr/share/drush/commands/drush_make':
-      ensure  => directory,
-      recurse => true,
-      purge   => true,
-      source  => '/tmp/drush_make',
-      require => Exec['fetch-drush-make'],
+#    '/usr/share/drush/commands/drush_make':
+#      ensure  => directory,
+#      recurse => true,
+#      purge   => true,
+#      source  => '/tmp/drush_make',
+#      require => Exec['fetch-drush-make'],
   }
 
   file { '/usr/local/bin/drush':
